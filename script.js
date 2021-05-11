96adfa783915833754c93ec67a9e2d1d6075bb35
Carl <carlbrainegebe@gmail.com>
2021-05-10 21:21:23 -0500

    add script.js, connect to index.html

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript"; 
-document.querySelector('body').appendChild(h2); 
+//create click alerts for the following:     
+    //  <label><input type="radio" name="fav" value="jer">Jerry</label>
+    //  <label><input type="radio" name="fav" value="geo">George</label>
+    //  <label><input type="radio" name="fav" value="ela">Elaine</label>
+    //  <label><input type="radio" name="fav" value="kra">Kramer</label>
+
+    const jerryInput = document.getElementsByTagName("label")[0];
+    const georgeInput = document.getElementsByTagName("label")[1];
+    const elaineInput = document.getElementsByTagName("label")[2];
+    const kramerInput = document.getElementsByTagName("label")[3];
+
+    function clickAlertKramer() {
+        alert("Congragulations, you picked the right answer!");
+    };
+
+    function clickAlertElaine() {
+        alert("Care to dance?");
+    };
+
+    function clickAlertGeorge() {
+        alert("A rare victory for Costanza");
+    };
+
+    function clickAlertJerry() {
+        alert("Your selection of Jerry was evened out by others' selection of the others");
+    };
+
+
+    jerryInput.addEventListener("click", clickAlertJerry);
+    kramerInput.addEventListener("click", clickAlertKramer);
+    elaineInput.addEventListener("click", clickAlertElaine);
+    georgeInput.addEventListener("click", clickAlertGeorge);
+
+   
